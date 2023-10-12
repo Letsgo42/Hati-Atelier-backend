e0f598fb2069ebf1d26c41d47c54b1b4a540a530
luc-lenovo <ksluccio@gmail.com>
2023-10-12 10:25:18 +0200

    Initial commit. Npm init. Install packages. Add index.js. Add logger.

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3000
+const logger = require('./tools/logger')
+
+// CONNECTION TO MONGODB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+}).then(() => logger.log('info', 'Connection to MongoDB successfull'))
+  .catch(() => logger.error('Connection to MongoDB failed !'))
+
+// NEW INSTANCE OF EXPRESS
+const app = express()
+
+// IMPORT MODELS
+require('./models/user')
+require('./models/book')
+
+// IMPORT ROUTES
+const userRouter = require('./routes/user.js')
+const bookRouter = require('./routes/book.js')
+
+// USE EXPRESS MIDDLEWARE
+app.use(express.json())
+
+app.use('/users', userRouter)
+app.use('/books', bookRouter)
+
+// CREATE SERVER
+app.listen(PORT, () => {
+  logger.log('info', `Server is running on http://localhost:${PORT}`)
+})
---
+++ tools/logger.js
@@ -0,0 +1,28 @@
+const { createLogger, format, transports } = require('winston')
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json(),
+    format.prettyPrint()
+  ),
+  defaultMeta: { service: 'Machettes' },
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.simple()
+      )
+    }),
+    new transports.File({ filename: '../logs/Machettes-errors.log', level: 'error' }),
+    new transports.File({ filename: '../logs/logs.txt' })
+  ]
+})
+
+module.exports = logger
