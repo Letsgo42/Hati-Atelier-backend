95c246871ae8d25b2c6faed36ed39f827ea9d210
luc-lenovo <ksluccio@gmail.com>
2023-10-12 11:29:24 +0200

    Add asyncErrorhandler

--- routes/book.js
+++ routes/book.js
@@ -1,32 +1,33 @@
 // IMPORT MODULES
 const express = require('express')
-const router = express.Router()
 const asyncHandler = require('express-async-handler')
 const jwt = require('jsonwebtoken');
 const logger = require('../tools/logger')
+const asyncErrorHandler = require('../tools/asyncErrorHandler');
 
 const BookModel = require('../models/book')
+const router = express.Router()
 
 // POST NEW BOOK // ADMIN ONLY
-router.post('/', asyncHandler(async (req, res, next) => {
+router.post('/', asyncErrorHandler(async (req, res, next) => {
   const book = await BookModel.create();
   res.send(book)
 }))
 
 // GET ALL BOOKS // AUTH USERS ONLY
-router.get('/', asyncHandler(async (req, res, next) => {
+router.get('/', asyncErrorHandler(async (req, res, next) => {
   const books = await BookModel.findAll();
   res.send(books)
 }))
 
 // GET ONE BOOK // AUTH USERS ONLY
-router.get('/:id', asyncHandler(async (req, res, next) => {
+router.get('/:id', asyncErrorHandler(async (req, res, next) => {
   const book = await BookModel.findOne();
   res.send(book)
 }))
 
 // DELETE ONE BOOK // ADMIN ONLY
-router.delete('/:id', asyncHandler(async (req, res, next) => {
+router.delete('/:id', asyncErrorHandler(async (req, res, next) => {
   const book = await BookModel.findOneAndDelete();
   res.send(book)
 }))
--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,21 @@
 // IMPORT MODULES
 const express = require('express')
-const router = express.Router()
 const asyncHandler = require('express-async-handler')
 const jwt = require('jsonwebtoken');
 const logger = require('../tools/logger')
+const asyncErrorHandler = require('../tools/asyncErrorHandler');
 
 const UserModel = require('../models/user')
+const router = express.Router()
 
 // REGISTER NEW USER //
-router.post('/register', asyncHandler(async (req, res, next) => {
+router.post('/register', asyncErrorHandler(async (req, res, next) => {
   const users = await UserModel.findAll();
   res.send(users)
 }))
 
 // LOGIN USER // RETURN JWT
-router.post('/login', asyncHandler(async (req, res, next) => {
+router.post('/login', asyncErrorHandler(async (req, res, next) => {
   const users = await UserModel.findAll();
   res.send(users)
 }))
